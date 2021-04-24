9f24ca085ab060ac0001282a9c7c8b260b888149
zx.xiao <xzxfirst@163.com>
2021-04-24 16:35:00 +0800

    feat:add

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,11 +1,3 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 console.log('hello zx morning')
-
-// const importLocal = require('import-local')
-
-// if (importLocal(__filename)) {
-//   require('npmlog').info('cli', '正在使用 zx-cli-dev 本地版本')
-// } else {
-//   require('../lib')(process.argv.slice(2))
-// }
